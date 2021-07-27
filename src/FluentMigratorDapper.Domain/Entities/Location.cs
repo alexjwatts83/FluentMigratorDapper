1feb16a065365d12e1a92eb05ccbcb16f5946f4d
Alex Watts <alexjwatts83@gmail.com>
2021-07-27 22:49:23 +1000

    change locations id to a string and added a new end point for locations

--- src/FluentMigratorDapper.Domain/Entities/Location.cs
+++ src/FluentMigratorDapper.Domain/Entities/Location.cs
@@ -2,7 +2,7 @@
 {
     public class Location
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string State { get; set; }
         public string City { get; set; }
