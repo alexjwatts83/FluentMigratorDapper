128dec949ecd00e82c02b6fc7000027497dfe7b5
Alex Watts <alexjwatts83@gmail.com>
2021-07-28 14:55:33 +1000

    moved RunDbMigrations to its own class

---
+++ src/FluentMigratorDapper.WebUI/Extensions/RunDbMigrations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+using FluentMigratorDapper.Infrastructure.Persistence.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FluentMigratorDapper.WebUI
+{
+
+    internal static class RunDbMigrations
+    {
+        internal static void Run(IHost host)
+        {
+            // Run db Migrations
+            var dbConnectionString = string.Empty;
+            var masterDb = string.Empty;
+            var mainDbName = string.Empty;
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var config = services.GetRequiredService<IConfiguration>();
+                dbConnectionString = config.GetConnectionString("Database");
+                masterDb = config.GetConnectionString("Master");
+                mainDbName = config.GetConnectionString("MainDbName");
+            }
+            var serviceProvider = CreateServices(dbConnectionString);
+
+            EnsureDatabase(masterDb, mainDbName);
+
+            // Put the database update into a scope to ensure that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static void EnsureDatabase(string connectionString, string name)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("name", name);
+            using var connection = new SqlConnection(connectionString);
+            var records = connection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+            if (!records.Any())
+            {
+                connection.Execute($"CREATE DATABASE {name}");
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices(string dbConnectionString)
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .Configure<AssemblySourceOptions>(x => x.AssemblyNames = new[] { typeof(AddLocations).Assembly.GetName().Name })
+                .ConfigureRunner(rb => rb
+                    // Add SQL Server support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString(dbConnectionString)
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddLocations).Assembly)
+                        .For.Migrations()
+                        .For.EmbeddedResources())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .Configure<FluentMigratorLoggerOptions>(options =>
+                {
+                    options.ShowSql = true;
+                    options.ShowElapsedTime = true;
+                })
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+    }
+}
