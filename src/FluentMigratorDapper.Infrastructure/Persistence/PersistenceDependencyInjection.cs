4a55c486ccc3c35c0ee0f7e3b10375670cc7cd18
Alex Watts <alexjwatts83@gmail.com>
2021-07-30 21:16:00 +1000

    removed unused function

--- src/FluentMigratorDapper.Infrastructure/Persistence/PersistenceDependencyInjection.cs
+++ src/FluentMigratorDapper.Infrastructure/Persistence/PersistenceDependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using FluentMigratorDapper.Application.Interfaces;
 using FluentMigratorDapper.Infrastructure.Persistence.Repositories;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,12 +7,6 @@ namespace FluentMigratorDapper.Infrastructure.Persistence
 {
     public static class PersistenceDependencyInjection
     {
-        public static IApplicationBuilder ConfigureErrorUsing(this IApplicationBuilder app)
-        {
-
-            return app;
-        }
-
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<ConnectionStringSettings>(configuration.GetSection(ConnectionStringSettings.Section));
