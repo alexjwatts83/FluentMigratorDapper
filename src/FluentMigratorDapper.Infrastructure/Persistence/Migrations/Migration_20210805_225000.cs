51f4ffe4473e74d84625a8880c7f57f6c3bd3d6b
Alex Watts <alexjwatts83@gmail.com>
2021-08-06 07:44:15 +1000

    moved out migrations

---
+++ src/FluentMigratorDapper.Infrastructure/Persistence/Migrations/Migration_20210805_225000.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using FluentMigrator;
+
+namespace FluentMigratorDapper.Infrastructure.Persistence.Migrations
+{
+
+    [Migration(20210805_225000)]
+    public class AddFestivalEventsTables : Migration, INonProductionMigration
+    {
+        public override void Up()
+        {
+            var sql = @"
+CREATE TABLE [dbo].[Festivals_Events](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[FestivalID] [nvarchar](3) NULL,
+	[ReferenceID] [nvarchar](255) NULL,
+	[LocationID] [nvarchar](3) NULL,
+	[Type] [nvarchar](255) NULL,
+	[Name] [nvarchar](255) NULL,
+	[ShortDescription] [nvarchar](max) NULL,
+	[LongDescription] [nvarchar](max) NULL,
+	[Thumbnail] [nvarchar](255) NULL,
+	[Link] [nvarchar](255) NULL,
+	[Date] [datetime] NULL,
+	[active] [bit] NOT NULL,
+ CONSTRAINT [PK_Festivals_Events] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO";
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Festivals_Events");
+        }
+    }
+}
---
+++ src/FluentMigratorDapper.Infrastructure/Persistence/Migrations/Migration_20210805_225500.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using FluentMigrator;
+
+namespace FluentMigratorDapper.Infrastructure.Persistence.Migrations
+{
+
+    [Migration(20210805_225500)]
+    public class AddSponsorColsToFestivals_Events : Migration, INonProductionMigration
+    {
+        public override void Up()
+        {
+            if (!Schema.Table("Festivals_Events").Column("Sponsor").Exists())
+            {
+                Alter.Table("Festivals_Events")
+                    .AddColumn("Sponsor").AsString(255).Nullable();
+            }
+
+            if (!Schema.Table("Festivals_Events").Column("SponsorLink").Exists())
+            {
+                Alter.Table("Festivals_Events")
+                    .AddColumn("SponsorLink").AsString(255).Nullable();
+            }
+        }
+
+        public override void Down()
+        {
+            // do nothing
+        }
+    }
+}
